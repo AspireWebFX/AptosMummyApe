baade881306322417d201b501e0456e3e558f068
AspireWebFX <philipxpromotes0@gmail.com>
2022-10-29 12:13:28 +0800

    yes

--- mint-site/helpers/candyMachineInfo.js
+++ mint-site/helpers/candyMachineInfo.js
@@ -1,4 +1,4 @@
-export const candyMachineAddress = "0xb2d27c4d1f98047c1491ed6d055787d3d921d2f0c810b857716b685172617094";
+export const candyMachineAddress = "0x6f5395850d595754f0737155e65085ea7006624b09ea1370af07ff84a2183a0f";
 export const collectionName = "Aptos-MummyApe"; // Case sensitive!
 export const collectionCoverUrl = "https://cloudflare-ipfs.com/ipfs/QmdVACsmYfhGXQAfkv9CT5kewsLgssSvKhs1LfYFcg8VNe";
 export const mode = "dev"; // "dev" or "test" or "mainnet"
